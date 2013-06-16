4bcdc60062221c320d0d2d030cf188d57d27411a
christianrochford <rochfordchristian@gmail.com>
2013-06-16 12:38:01 +0100

    created admin.php

---
+++ admin/admin.php
@@ -0,0 +1,30 @@
+<?php?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Michael Soelberg | Contact</title>
+    
+    <meta name="description" content="The work of artist Michael Soelberg">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" href="css/style.css">
+    
+    <script src="js/modernizr.custom.71203.js"></script>
+</head>
+<body>
+    
+    <?php include 'php/inc/header.inc.php'; ?>
+    
+    <section id="login">
+        <form action="#" method="post">
+            <label id="Username">Username:</label>
+            <input id="Username" name="Username">
+            <label id="Password">Password:</label>
+            <input id="Password" name="Password">
+        </form>
+    </section>
+    
+    <?php include 'php/inc/footer.inc.php'; ?>
+
+</body>
+</html>
